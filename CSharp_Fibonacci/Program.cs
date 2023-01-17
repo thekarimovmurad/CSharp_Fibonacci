56f4a5fa0e8b49361350a82c56400e5dd98c1951
Murad Karimov <thekarimovmurad@outlook.com>
2023-01-17 22:27:46 +0100

    Commit

--- CSharp_Fibonacci/Program.cs
+++ CSharp_Fibonacci/Program.cs
@@ -11,15 +11,27 @@ namespace CSharp_Fibonacci
         static void Main(string[] args)
         {
             Console.Write("Enter index: ");
-            int index= Convert.ToInt32(Console.ReadLine());
-            int[] fibonacci= new int[index + 1];
-            fibonacci[0]=0;
-            fibonacci[1]=1;
+            int index = Convert.ToInt32(Console.ReadLine());
+            int[] fibonacci = new int[index + 1];
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
             for (int i = 2; i <= index; i++)
             {
                 fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
             }
             Console.WriteLine(fibonacci[index]);
+            //Console.Write("Enter index: ");
+            //int index = Convert.ToInt32(Console.ReadLine());
+            //for (int i = 0; i < index; i++)
+            //{
+            //    Console.WriteLine(Fibonacci(index));
+            //}
         }
+        //static int Fibonacci(int i)
+        //{
+        //    if (i == 0) return 0;
+        //    if (i == 1) return 1;
+        //    return Fibonacci(i - 2) + Fibonacci(i - 1);
+        //}
     }
 }
